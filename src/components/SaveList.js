917683b01a68ae21afbd2bd442f1522a7a293439
Tamurov_Dmitrii <dtamurov19@gmail.com>
2023-03-22 01:03:25 +0300

    homework final

--- src/components/SaveList.js
+++ src/components/SaveList.js
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 
 const SaveList = () => {
     const isLoggedIn = useSelector((state) => state.isLoggedIn)
+    const coffee = useSelector((state) => state.coffee)
+    const sugar = useSelector((state) => state.sugar)
     const dispatchFunc = useDispatch();
-    console.log(isLoggedIn)
 
-    const isValid = () => {
-        dispatchFunc({type: 'LOGIN'})
+    const SAVE_LIST = 'SAVE_LIST';
+
+    const CLEAR_LIST = () => {
+        dispatchFunc({ type: 'CLEAR_LIST'})
+
     }
+    const saveList = (coffee, sugar) => {
+        localStorage.setItem('coffee', coffee);
+        localStorage.setItem('sugar', sugar);
+        return {
+          type: SAVE_LIST,
+          payload: { coffee, sugar },
+        };
+      };
+
+      const handleSave = () => {
+        dispatchFunc(saveList(coffee, sugar));
+      };
 
     const buttonsFunc = (valid) => {
         if(valid) {
             return (
                 <div className='save'>
-                    <button>SAVE</button>
-                    <button>CLEAR</button>
+                    <button onClick={handleSave}>SAVE</button>
+                    <button onClick={CLEAR_LIST}>CLEAR</button>
                 </div>
             )
         }
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,9 @@
 import { legacy_createStore } from 'redux'
+import { useEffect } from 'react';
 
 const initialState = {
-    coffee: 0, 
-    sugar: 0,
+    coffee: 0 + +localStorage.getItem('coffee'), 
+    sugar: 0 + +localStorage.getItem('sugar'),
     isLoggedIn: false,
 }
 
@@ -12,9 +13,18 @@ const INCREASE_COFFEE = 'INCREASE_COFFEE';
 const INCREMENT_SUGAR = 'INCREMENT_SUGAR';
 const DECREMENT_SUGAR = 'DECREMENT_SUGAR';
 const INCREASE_SUGAR = 'INCREASE_SUGAR';
+const CLEAR_LIST = 'CLEAR_LIST';
 
 const couterReducer = (state = initialState, action) => {
-    console.log(state)
+    if(action.type === CLEAR_LIST) {
+        localStorage.removeItem('coffee')
+        localStorage.removeItem('sugar')
+        return {
+            coffee: 0,
+            sugar: 0,
+        }
+
+    }
     switch(action.type) {
         case INCREMENT_COFFEE:
             return {
@@ -52,9 +62,7 @@ const couterReducer = (state = initialState, action) => {
                 sugar: state.sugar + 10,
                 isLoggedIn: state.isLoggedIn,
             }
-    
     }
-    console.log(state)
 
     if(action.type === 'LOGIN'){
         return {
